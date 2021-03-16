1e88e53cf44498be3e8a03ac64873cfad04fa167
Jefferson <jefry04@gmail.com>
2021-03-16 10:16:03 -0500

    ready

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ' ',
+      count: 0,
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      count: e.target.value.length,
+      name: e.target.value,
+    });
+  };
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea
+          rows="3"
+          value={this.state.name}
+          onChange={this.handleChange}
+        ></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
